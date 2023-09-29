b6fe5a0216296501ae3174f7e485abb6654cfb14
Sh1v4nk <shivankpandey113@gmail.com>
2023-09-29 21:32:14 +0530

    Changed var to const

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function performSearch() {
-  var searchTerm = document.querySelector(".input").value;
+  const searchTerm = document.querySelector(".input").value;
   window.location.href =
     "https://www.google.com/search?q=" + encodeURIComponent(searchTerm);
 }
